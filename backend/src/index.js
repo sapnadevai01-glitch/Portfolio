1c1c4ca231ce8241983b334c088a1db1d4d7f7df
sapnadevai01-glitch <sapnadev.ai01@gmail.com>
2026-07-26 15:06:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,9 @@ const contactLimiter = rateLimit({
 });
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  origin:"https://portfolio-ui-7ays.onrender.com"
+));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
